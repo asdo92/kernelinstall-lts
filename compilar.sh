f3504359043e8b0d1dcb56d88da24fa86dd41923
asdo92 <asdo92@duck.com>
2026-02-05 16:07:58 +0100

    Se adapta el script de compilación

--- compilar.sh
+++ compilar.sh
@@ -3,18 +3,18 @@
 # License GNU GPL 3.0 (See License for more Information)
 
 # Este es un script para quienes deseen compilar el programa. Si no querés compilarlo podes simplemente correr
-# ./kernel-installer
+# ./kernel-installer-lts
 
 compilar() {
 sudo apt install -y gcc make gettext libncurses-dev pkg-config libncursesw5-dev
 make clean
 make
 sudo make install
-kernel-installer
+kernel-installer-lts
 }
 
 
-echo -e "\nEste es un script para quienes deseen compilar el programa.\nSi no lo querés compilar, podés simplemente ejecutar ./kernel-installer.\nTambién requiere sudo. Si tu sistema no tiene sudo, correlo como root\n\n¿Deseas Compilar de todos modos? [S/N]"
+echo -e "\nEste es un script para quienes deseen compilar el programa.\nSi no lo quieres compilar, puedes simplemente ejecutar ./kernel-installer-lts.\nTambién requiere sudo. Si tu sistema no tiene sudo, ejecutalo como root\n\n¿Deseas Compilar de todos modos? [S/N]"
 read i
 
 case $i in
